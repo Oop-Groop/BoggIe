383c2abf5e97f99f4e37f49e1b199d09c3d42418
Zeale <zeale@dusttoash.org>
2019-11-24 15:53:08 -0600

    Add main cls

---
+++ src/oopgroop/projects/boggIe/app/BoggIe.java
@@ -0,0 +1,19 @@
+package oopgroop.projects.boggIe.app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public final class BoggIe extends Application{
+	
+	public BoggIe() {	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.show();
+	}
+
+}
