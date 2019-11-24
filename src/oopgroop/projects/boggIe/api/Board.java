faaf20b1ac2b8740a5b81497c98174a550cb3786
Zeale <zeale@dusttoash.org>
2019-11-24 16:33:27 -0600

    Move Dice class to Board

--- src/oopgroop/projects/boggIe/api/Board.java
+++ src/oopgroop/projects/boggIe/api/Board.java
@@ -1,15 +1,47 @@
 package oopgroop.projects.boggIe.api;
 
-public class Board<T> {
+import java.util.Random;
 
-	private final T[][] board;
+import javafx.scene.layout.GridPane;
 
-	public T getTile(int x, int y) {
+public class Board extends GridPane {
+
+	private final Dice[][] board;
+
+	public Dice getTile(int x, int y) {
 		return board[x][y];
 	}
 
-	@SuppressWarnings("unchecked")
 	public Board(int length, int height) {
-		board = (T[][]) new Object[length][height];
+		board = new Dice[length][height];
+	}
+
+	public class Dice {
+		private char[] letters;
+		private String letter;
+
+		private Dice(String s, int x, int y) {
+			this.letters = s.toCharArray();
+		}
+
+		public String rollDice() {
+			Random rand = new Random();
+			char letter = this.letters[rand.nextInt(6)];
+			if (letter == 'Q') {
+				this.letter = "Qu";
+				return "Qu";
+			}
+			this.letter = String.valueOf(letter);
+			return this.letter;
+		}
+
+		public String getLetter() {
+			return letter;
+		}
+
+		public char[] getLetters() {
+			return letters;
+		}
+
 	}
 }
