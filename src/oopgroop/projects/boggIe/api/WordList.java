6c8c077f88cddadb1234428393dc44b2552994b3
Miggy Reyes <ynigoreyes@gmail.com>
2019-11-24 17:16:51 -0600

    adding the class

---
+++ src/oopgroop/projects/boggIe/api/WordList.java
@@ -0,0 +1,11 @@
+package oopgroop.projects.boggIe.api;
+
+/**
+ * Word list is a trie that contains all the words for the
+ * game as well as their scores.
+ *
+ * Here is a link of what a Trie is
+ * https://www.hackerearth.com/practice/data-structures/advanced-data-structures/trie-keyword-tree/tutorial/
+ */
+public class WordList {
+}
