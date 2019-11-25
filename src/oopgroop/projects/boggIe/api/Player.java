48cf2346f0e477b6bb866066f29739d4c1d45be1
James <fugang.deng@ttu.edu>
2019-11-24 18:53:07 -0600

    add Timer to Player class but not tested yet

--- src/oopgroop/projects/boggIe/api/Player.java
+++ src/oopgroop/projects/boggIe/api/Player.java
@@ -1,12 +1,25 @@
 package oopgroop.projects.boggIe.api;
 import java.util.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /*
  * This is the player class
  */
-public class Player {
+public class Player extends TimerTask {
 	private int score;
 	Set<String> guessedWords = new HashSet<>();
+	//For the timer
+	private Timer myTimer;
+	
+	public void Reminder (int seconds){
+		myTimer.schedule(new Player(), seconds*1000);
+	}
+	
+	public void run() {
+		System.out.println("Time's up!");
+		myTimer.cancel();
+	}
 	
 	public Player() {
 		this.score = 0;
