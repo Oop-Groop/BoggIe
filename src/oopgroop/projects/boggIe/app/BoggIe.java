22945379dbddb575484c5fc30942997b3d9aa371
Zeale <zeale@dusttoash.org>
2019-11-24 19:41:36 -0600

    Update GUI

--- src/oopgroop/projects/boggIe/app/BoggIe.java
+++ src/oopgroop/projects/boggIe/app/BoggIe.java
@@ -1,10 +1,18 @@
 package oopgroop.projects.boggIe.app;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import oopgroop.projects.boggIe.api.Board;
 
@@ -21,8 +29,23 @@ public final class BoggIe extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.show();
 		Board board = new Board(4, 4);
-		board.getRoot().setBackground(new Background(new BackgroundFill(Color.DARKRED, null, null)));
-		primaryStage.setScene(new Scene(board.getRoot()));
+
+		class Thing {// The controller.
+			public @FXML Text playerName;
+			public @FXML TextField input;
+			public @FXML Button submit;
+		}
+		Thing thing = new Thing();
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("TheUnnamedGUIPart.fxml"));
+		loader.setController(thing);
+		HBox.setHgrow(board.getRoot(), Priority.ALWAYS);
+		board.getRoot().setMinSize(400, 400);
+
+		HBox box = new HBox(20, board.getRoot(), loader.load());
+		box.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
+		box.setAlignment(Pos.CENTER);
+
+		primaryStage.setScene(new Scene(box));
 	}
 
 }
